05a0adbfdb66093549e695d4720528e3fe817818
saaratrix <saaratrix@gmail.com>
2023-10-01 22:13:45 +0300

    Improved uploading UX and validation. Added max length, improved layout a little and loading spinner. Also added share link to copy.

--- client/public/src/file-uploader.ts
+++ client/public/src/file-uploader.ts
@@ -3,7 +3,7 @@ import { FileInputEvent, fileInputEvent } from "./file-events.js";
 export interface UploadInfo {
   extensions: string[];
   maxlengthFile: number;
-  maxlengthUser: number;
+  maxlengthSecret: number;
 }
 
 export interface UploadResponse {
@@ -11,41 +11,79 @@ export interface UploadResponse {
 }
 
 export class FileUploader {
-    private uploadInfo: UploadInfo | undefined;
-    canUpload = false;
+  private uploadInfo: UploadInfo | undefined;
+  canUpload = false;
 
-    selectedFile: File | undefined;
-    selectedKey: string = '';
+  filename: string = '';
+  selectedFile: File | undefined;
+  selectedKey: string = '';
 
   constructor() {
     window.addEventListener(fileInputEvent, (event) => this.onFileInput(event as CustomEvent<FileInputEvent>));
+    document.getElementById('upload-btn')?.addEventListener('click', () => this.onUploadClicked());
+    document.getElementById('filename')!.addEventListener('input', (event) => this.onFilenameChange(event));
 
-    document.getElementById('upload-btn')?.addEventListener('click', () => this.onSubmit());
+    this.initSecrets();
+    this.initLinkSharing();
+    this.initUploadingInfo();
+  }
+
+  initSecrets(): void {
     const secretsElement = document.getElementById('secret') as HTMLInputElement;
     secretsElement.addEventListener('input', (event) => {
       this.selectedKey = (event.target as HTMLInputElement).value;
       try {
         localStorage.setItem('key', this.selectedKey);
-      } catch {}
+      } catch {
+      }
     });
 
     try {
       this.selectedKey = localStorage.getItem('key') ?? '';
       secretsElement.value = this.selectedKey;
-    } catch { }
-
-    this.getUploadInfo(0).then((uploadInfo) => {
-        this.uploadInfo = uploadInfo;
-        this.canUpload = true;
-        console.log('can now upload!');
-        this.setUploadButtonStatus();
-    })
-    .catch(() => this.canUpload = false);
+    } catch {
+    }
   }
 
-  setUploadButtonStatus() {
+  initLinkSharing(): void {
+    const linkShareElement = document.getElementById('link-share') as HTMLElement;
+    const copyButton = linkShareElement.querySelector<HTMLElement>('.link-copy-btn') as HTMLElement;
+    copyButton.addEventListener('click', () => this.copyLinkToClipboard());
+  }
+
+  initUploadingInfo(): void {
+    this.getUploadInfo(0).then((uploadInfo) => {
+      this.uploadInfo = uploadInfo;
+      this.canUpload = true;
+      this.setUploadButtonStatus();
+      this.setupValidation();
+    })
+      .catch(() => this.canUpload = false);
+  }
+
+  setupValidation(): void {
+    const uploadInfo = this.uploadInfo!;
+    (document.getElementById('secret') as HTMLInputElement).maxLength = uploadInfo.maxlengthSecret;
+    (document.getElementById('filename') as HTMLInputElement).maxLength = uploadInfo.maxlengthFile;
+  }
+
+  setUploadButtonStatus(): void {
     const uploadButton = document.getElementById('upload-btn') as HTMLButtonElement;
-    if (!this.canUpload || !this.selectedFile ) {
+    if (!this.canUpload || !this.selectedFile) {
+      uploadButton.disabled = true;
+      return;
+    }
+
+    let found = false;
+    const lowerFilename = this.filename.toLowerCase();
+    for (const extension of this.uploadInfo!.extensions) {
+      if (lowerFilename.endsWith(extension)) {
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
       uploadButton.disabled = true;
       return;
     }
@@ -82,30 +120,87 @@ export class FileUploader {
     }
   }
 
+  private onFilenameChange(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    this.setFilename(target.value, false);
+  }
+
+  private setFilename(name: string, updateInput: boolean): void {
+    this.filename = name;
+    if (updateInput) {
+      (document.getElementById('filename') as HTMLInputElement).value = name;
+    }
+
+    this.setUploadButtonStatus();
+    this.onShareLinkChanged('');
+  }
+
+  private onShareLinkChanged(link: string): void {
+    const linkShareElement = document.getElementById('link-share') as HTMLElement;
+    const linkTextElement = linkShareElement.querySelector<HTMLSpanElement>('.link-text') as HTMLSpanElement;
+
+    if (link === linkTextElement.innerHTML) {
+      return;
+    }
+
+    !link ? linkShareElement.classList.add('invisible') : linkShareElement.classList.remove('invisible');
+    linkTextElement.innerHTML = link;
+  }
 
   async onFileInput(event: CustomEvent<FileInputEvent>) {
     const file = event.detail;
     this.selectedFile = file;
 
+    let filename = file.name;
+    if (this.uploadInfo) {
+      let end = file.name.length;
+      let start = file.name.length <= this.uploadInfo.maxlengthFile ? 0 : file.name.length - this.uploadInfo.maxlengthFile;
+      filename = file.name.substring(start, end);
+    }
+
+    this.setFilename(filename, true);
     this.setUploadButtonStatus();
   }
 
 
-  private async onSubmit() {
+  private async onUploadClicked() {
     if (!this.selectedFile || !this.canUpload) {
       return;
     }
 
     const formData = new FormData();
     formData.set('file', this.selectedFile);
+    formData.set('filename', this.filename);
     formData.set('key', this.selectedKey);
+
+    const uploadButton = document.getElementById('upload-btn') as HTMLButtonElement;
+    uploadButton.classList.add('spinner');
+    uploadButton.disabled = true;
 
     const request = fetch('http://localhost:5001/upload', {
       method: 'PUT',
       body: formData,
     });
-    const response = await request;
-    const json = await response.json() as UploadResponse;
-    console.log(json);
+
+    try {
+      const response = await request;
+      const json = await response.json() as UploadResponse;
+      const url = `${location.href}/${json.url}`;
+      this.onShareLinkChanged(url);
+    } finally {
+      uploadButton.classList.remove('spinner');
+      uploadButton.disabled = false;
+    }
+  }
+
+  private copyLinkToClipboard() {
+    const linkShareElement = document.getElementById('link-share') as HTMLElement;
+    const linkTextElement = linkShareElement.querySelector<HTMLSpanElement>('.link-text') as HTMLSpanElement;
+    const text = linkTextElement.innerHTML;
+
+    if (!text) {
+      return;
+    }
+    navigator.clipboard.writeText(text)
   }
 }
