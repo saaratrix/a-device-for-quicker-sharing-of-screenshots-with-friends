770ef3fef48a3af5058a5e6f74517917c95074c1
saaratrix <saaratrix@gmail.com>
2023-10-14 19:19:40 +0300

    Fixed local.py origins.

--- server/local.py
+++ server/local.py
@@ -6,6 +6,7 @@ from app import create_app
 if __name__ == '__main__':
     app = create_app()
 
-    origins = os.environ.get("ORIGINS", '"http://localhost:63342","http://localhost:63343"').split(",")
+    origins = os.environ.get("ORIGINS", "http://localhost:63342,http://localhost:63343").split(",")
     CORS(app, origins=origins)
+    # CORS(app, origins=["http://localhost:63342"])
     app.run(debug=True, port=5001)
