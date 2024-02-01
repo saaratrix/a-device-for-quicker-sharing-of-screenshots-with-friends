2f2a0a61058ef45e915a7e822d34772009d380d6
saaratrix <saaratrix@gmail.com>
2024-02-01 21:25:20 +0200

    Added unicorn error logging, or at least registered it.

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,14 +1,17 @@
 import os
 import sys
-
-# Get the path of the directory the wsgi.py file is in
+import logging
+# Append the parent path or `server.` doesn't work in child modules.
 current_path = os.path.dirname(os.path.abspath(__file__))
-# Get the parent directory
 parent_path = os.path.dirname(current_path)
-# Add the parent directory to sys.path
 sys.path.append(parent_path)
 
 from src.app import create_app
 
 uploads_path = os.path.join(os.path.dirname(__file__), "file_uploads")
 app = create_app(uploads_path, True)
+
+if __name__ != "__main__":
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
